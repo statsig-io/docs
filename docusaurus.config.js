209117ec28362b71d25fe48b8d1fc23b9466a827
Jiakan Wang <77478330+jkw-statsig@users.noreply.github.com>
2021-06-08 14:31:31 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ module.exports = {
       copyright: `Copyright (c) ${new Date().getFullYear()} Statsig, Inc. | Thanks Docusaurus`,
     },
     prism: {
-      additionalLanguages: ['swift'],
+      additionalLanguages: ['swift', 'java', 'ruby'],
     }
   },
   presets: [
