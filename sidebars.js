8efd6de85fa257844a3f1e66d2d72df8b1c32969
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-06-16 14:18:00 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -7,7 +7,7 @@ module.exports = {
         'guides/first-dynamic-config',
         'guides/first-experiment',
         'guides/first-holdout',
-        'guides/usingEnvironments",
+        'guides/usingEnvironments',
       ],
     },
     {
