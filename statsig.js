6f2c6fc0f3ee580a278e8f6f68e18d0e90e3e926
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-09-03 17:35:09 -0700

    Add initial statsig plugin for docusaurus (#84)
    * First attempt at statsig plugin for docusaurus
    
    * update sdk version

---
+++ statsig.js
@@ -0,0 +1,14 @@
+ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+ 
+ export default (function () {
+   if (!ExecutionEnvironment.canUseDOM) {
+     return null;
+   }
+ 
+   return {
+     onRouteUpdate({location}) {
+       // Always refer to the variable on window in-case it gets overridden elsewhere.
+       window.statsig.logEvent('page_view', window.location.pathname);
+     },
+   };
+ })();
