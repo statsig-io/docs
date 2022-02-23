302b524816b618552ff88559f2cb6d4c61d06ae6
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-02-23 13:58:26 -0800

    Remove TOC items to fix build

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -6,7 +6,6 @@
  */
 import React from 'react';
 import clsx from 'clsx';
-import TOCItems from '@theme/TOCItems';
 import styles from './styles.module.css'; // Using a custom className
 // This prevents TOC highlighting to highlight TOCInline/TOCCollapsible by mistake
 
@@ -16,11 +15,6 @@ const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
 function TOC({className, ...props}) {
   return (
     <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
-      <TOCItems
-        {...props}
-        linkClassName={LINK_CLASS_NAME}
-        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
-      />
       <div className={clsx(styles.slackUpsell)}>
         <h4>Still Stuck?</h4>
         <p>
