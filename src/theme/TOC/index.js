bfb744df5af1b8b7121c89038212554360cfed09
vijaye-statsig <74588208+vijaye-statsig@users.noreply.github.com>
2022-02-23 10:33:19 -0800

    Show a 'Still Stuck' footer on all docs page and point people to Slack (#197)

---
+++ src/theme/TOC/index.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import clsx from 'clsx';
+import TOCItems from '@theme/TOCItems';
+import styles from './styles.module.css'; // Using a custom className
+// This prevents TOC highlighting to highlight TOCInline/TOCCollapsible by mistake
+
+const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
+const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
+
+function TOC({className, ...props}) {
+  return (
+    <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+      <TOCItems
+        {...props}
+        linkClassName={LINK_CLASS_NAME}
+        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
+      />
+      <div className={clsx(styles.slackUpsell)}>
+        <h4>Still Stuck?</h4>
+        <p>
+          Join our Slack community and chat with our Engineers and 
+          Data Scientists
+        </p>
+        <div style={{textAlign: "center"}}>
+          <a href="https://www.statsig.com/slack" className="button">
+            <img src="/img/slack.webp" />&nbsp;ASK AN ENGINEER
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TOC;
