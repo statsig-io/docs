68fa1f1a6bfae445ab20d4e1ad164c7e45bf8061
Jacob O'Quinn <jacob@statsig.com>
2022-06-07 15:35:05 -0700

    Render our apiDoc
    Able to embed rapidoc of our openApi 3.0 specs

--- src/pages/components/Rapidoc.js
+++ src/pages/components/Rapidoc.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import('rapidoc');
 
 export default function Rapidoc(props) {
-		let {apiUrl} = props
-		if (ExecutionEnvironment.canUseDOM) {
-				/// Make sure we only import the rapidoc web component in case we are on the client side
-				return (
-					<rapi-doc 
-							header-color={"#004876"}
-							nav-text-color={"#004876"}
-							primary-color={"#ff9e1b"}
-							nav-bg-color={"#ffffff"}
-							id="thedoc"
-							allow-search={false}
-							render-style={"read"}
-							allow-try={true}
-							show-header={false}
-							allow-authentication={true}
-							regular-font="Roboto"
-							use-path-in-nav-bar={true}
-							style={{height: "80vh", width: "100%"}}
-							mono-font="monospace" theme={"light"}
-							spec-url={apiUrl}/>
-				)}
-		return <div/>
+  let {apiUrl} = props
+
+	const rapidoc = <rapi-doc 
+											header-color={"#004876"}
+											nav-text-color={"#004876"}
+											primary-color={"#ff9e1b"}
+											nav-bg-color={"#ffffff"}
+											id='rapidoc'
+											allow-search={false}
+											render-style={"read"}
+											allow-try={true}
+											show-header={false}
+											allow-authentication={true}
+											regular-font="Roboto"
+											use-path-in-nav-bar={true}
+											style={{height: "80vh", width: "100%"}}
+											mono-font="monospace" theme={"light"}/>
+
+	useEffect(() => {
+		setTimeout(() => {
+			var data = require('../../../docs/console-api/openapi/main.json');
+
+			const el = document.getElementById("rapidoc");
+			el.loadSpec(data)
+		}, 30)
+	}, []);
+
+  if (ExecutionEnvironment.canUseDOM) {
+    /// Make sure we only import the rapidoc web component in case we are on the client side
+    return (
+			rapidoc
+		)
+	}
+  return <div/>
+
 }
