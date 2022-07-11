26df0d0a0ea1f96eb73a45eb0cf07d0ad871b8c6
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-07-11 16:59:02 -0700

    update js client sdk version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ module.exports = {
               {
                 tagName: "script",
                 attributes: {
-                  src: "https://cdn.jsdelivr.net/npm/statsig-js@4.12.0-beta.0/build/statsig-prod-web-sdk.min.js",
+                  src: "https://cdn.jsdelivr.net/npm/statsig-js@4.17.0-beta.1/build/statsig-prod-web-sdk.min.js",
                 },
               },
               {
