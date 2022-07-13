70cd9a28345c755fc58d00f8b6ec2b532e806847
jacob-statsig <jacob@statsig.com>
2022-07-13 13:40:49 -0700

    'all endpoints' order matches console

--- src/components/Rapidoc.js
+++ src/components/Rapidoc.js
@@ -3,7 +3,8 @@ import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import Models from "../../docs/console-api/models/index";
 import { useColorMode } from '@docusaurus/theme-common';
 
-const supportedEntities = ["autotunes", "dynamic-configs","gates","holdouts","segments","experiments"]
+// In the order they will appear on All Endpoint's tab
+const supportedEntities = ["gates", "experiments", "dynamic-configs", "segments", "autotunes", "holdouts" ]
 
 function updateCodeSnippets(data, entity) {
   let snippet;
