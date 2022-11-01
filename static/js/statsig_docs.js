d4f272f8f424a4804036a11158f5544b11281fef
Vijaye Raji <vijaye@statsig.com>
2022-11-01 10:47:38 -0700

    Adding Koala to Docs site

--- static/js/statsig_docs.js
+++ static/js/statsig_docs.js
@@ -26,7 +26,15 @@ function initStatsig() {
   });
 }
 
+function setupKoala() {
+  window.ko = window.ko || [];
+  ["identify","track", "removeListeners", "open", "on", "off", "qualify", "ready"].forEach(function(t) {
+    ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}
+  });
+}
+
 initStatsig();
+setupKoala();
 window.addEventListener('load', function () {
   wireClickHandlers('a');
 });
