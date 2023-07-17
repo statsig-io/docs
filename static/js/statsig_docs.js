31fa9120c8c98e35e521d599b7ce96487563145b
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2023-07-17 15:11:02 -0700

    remove at top level

--- static/js/statsig_docs.js
+++ static/js/statsig_docs.js
@@ -2,28 +2,28 @@ function wireClickHandlers(tagName) {
   const elements = document.getElementsByTagName(tagName);
   for (let ii = 0; ii < elements.length; ii++) {
     const el = elements[ii];
-    // el.addEventListener('click', () => {
-    //   statsig.logEvent(
-    //     'link_click', 
-    //     el.pathname, 
-    //     { page : window.location.href, referrer: document && document.referrer }
-    //   );
-    // });
+    el.addEventListener('click', () => {
+      statsig.logEvent(
+        'link_click', 
+        el.pathname, 
+        { page : window.location.href, referrer: document && document.referrer }
+      );
+    });
   }
 }
 
 function initStatsig() {
-  // statsig.initialize(
-  //   'client-oJY6hTJeduhEN2bf6fh6unHvxIk9UsjS99BlO4owh0r', 
-  //   null, 
-  //   { environment: { tier: window.statsigTier }}
-  // ).then((err) => {
-  //   statsig.logEvent(
-  //     'page_view', 
-  //     window.location.pathname, 
-  //     { page : window.location.href, referrer: document && document.referrer }
-  //   );
-  // });
+  statsig.initialize(
+    'client-oJY6hTJeduhEN2bf6fh6unHvxIk9UsjS99BlO4owh0r', 
+    null, 
+    { environment: { tier: window.statsigTier }}
+  ).then((err) => {
+    statsig.logEvent(
+      'page_view', 
+      window.location.pathname, 
+      { page : window.location.href, referrer: document && document.referrer }
+    );
+  });
 }
 
 function setupKoala() {
@@ -33,8 +33,8 @@ function setupKoala() {
   });
 }
 
-initStatsig();
+// initStatsig();
 setupKoala();
-window.addEventListener('load', function () {
-  wireClickHandlers('a');
-});
+// window.addEventListener('load', function () {
+//   wireClickHandlers('a');
+// });
