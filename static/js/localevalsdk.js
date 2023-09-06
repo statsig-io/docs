86d7a9f81be29cad8461831011fc2ab98be87c90
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2023-09-06 12:09:20 -0700

    try catch for safety

--- static/js/localevalsdk.js
+++ static/js/localevalsdk.js
@@ -1,9 +1,12 @@
 async function setup() {
   if (typeof window !== 'undefined') {
     window.addEventListener('load', async function() {
+      try {
         await window.statsig.initializeAsync("client-LAx5juseYG9kxnB2vHLxFluaFmZVv9aAKPmw1NB8rps");
-    
         window.statsig.getExperiment({}, "a_a_test");
+      } catch (e) {
+        console.error(e);
+      }
     });
     
   }
