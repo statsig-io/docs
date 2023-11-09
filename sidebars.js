60a68494e11344d384f799748ff1d5ec7d3c67b6
Liz Obermaier <liz@statsig.com>
2023-11-09 15:44:26 -0800

    Add WHN monitor an experiment page

--- sidebars.js
+++ sidebars.js
@@ -76,6 +76,7 @@ module.exports = {
             "statsig-warehouse-native/features/cohort-metrics",
             "statsig-warehouse-native/features/id-resolution",
             "statsig-warehouse-native/features/entity-properties",
+            "statsig-warehouse-native/features/monitor-an-experiment",
           ],
         },
         {
