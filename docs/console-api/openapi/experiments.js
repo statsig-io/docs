69f11b9686a4b5d1d9e2e3774c3e5d4164eb1637
Jacob O'Quinn <jacob@statsig.com>
2024-02-05 15:31:05 -0800

    add pagination parameters (#1237)

--- docs/console-api/openapi/experiments.js
+++ docs/console-api/openapi/experiments.js
@@ -651,6 +651,24 @@ module.exports = {
             name: "layerID",
             description: "Filter by experiments inside of the selected layer",
           },
+          {
+            "schema": {
+              "type": "number",
+              "minimum": 1
+            },
+            "in": "query",
+            "name": "page",
+            "description": "Pagination page number (must also pass limit)"
+          },
+          {
+            "schema": {
+              "type": "number",
+              "minimum": 1
+            },
+            "in": "query",
+            "name": "limit",
+            "description": "Gates per pagination response"
+          }
         ],
       },
       parameters: [],
