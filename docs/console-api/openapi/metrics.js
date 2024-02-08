d2c0714137aaf4f89ef63eda396d4aecfed8f66e
Brian Do brian@statsig.com <>
2024-02-08 14:52:01 -0800

    update capi update metric docs
    show 'isVerified' as an option in request body for metric update endpoint
    
    ghstack-source-id: ed27824ac781d187bd2c2ee62d022864011689a2
    Pull Request resolved: https://github.com/statsig-io/docs/pull/1260

--- docs/console-api/openapi/metrics.js
+++ docs/console-api/openapi/metrics.js
@@ -996,6 +996,12 @@ module.exports = {
                       type: "string",
                     },
                   },
+                  isVerified: {
+                    type: "boolean",
+                    description: "This is used to display a \"verified\" icon next to this metric throughout the Statsig Console."
+                  }
+                }
+              },
                 },
               },
               examples: {
