c9eb88b7443fb97a208c44c15fde6ccdd9ff5612
Brock Lumbard <brock@statsig.com>
2024-02-21 13:28:29 -0800

    added ga4 tracking to docs site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,6 +128,15 @@ module.exports = {
           return {
             headTags: [
               {
+                tagName: 'script',
+                attributes: {
+                  type: 'text/javascript',
+                  async: true,
+                  src: 'https://www.googletagmanager.com/gtag/js?id=G-EM5RHE1RHW',
+                  // Add other attributes as needed, like 'nonce' if you use it
+                }
+              },
+              {
                 tagName: "script",
                 attributes: {
                   src: "/js/datadog.js",
