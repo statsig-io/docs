2b49490b664d3f9b0813e297c6fd535c5b65e2ca
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-03-08 12:58:55 -0800

    Js mono redux 2 (#1314)
    * adding js-mono
    
    * populating docs for js-mono
    
    * More js-mono docs
    
    * samples
    
    * embedding samples
    
    * adding js mono samples
    
    * use GitHubEmbed for js-mono snippets
    
    * moving statsig-js migration
    
    * adding more snippets
    
    * Update javascript-sdk.mdx

---
+++ src/components/GitHubEmbed.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import CodeBlock from "@theme/CodeBlock";
+
+function extractSnippet(input) {
+  const parts = input.split("\n");
+  const result = [];
+
+  let isAdding = false;
+
+  for (const line of parts) {
+    if (line.includes("<snippet>")) {
+      isAdding = true;
+    } else if (line.includes("</snippet>")) {
+      isAdding = false;
+    } else if (isAdding) {
+      result.push(line);
+    }
+  }
+
+  return result.join("\n");
+}
+
+export default function GitHubEmbed({ url, language }) {
+  const [content, setContent] = useState("// Loading...");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.text();
+        setContent(extractSnippet(data));
+      } catch (error) {
+        setContent(`// Failed to load example.\n// View on GitHub:\n// ${url}`);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return <CodeBlock language={language ?? "typescript"}>{content}</CodeBlock>;
+}
