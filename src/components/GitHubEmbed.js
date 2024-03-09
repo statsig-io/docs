39f80df259e6829cbc38be7fd5cfaa7cb8fcf5f4
Daniel Loomb <daniel@statsig.com>
2024-03-08 16:54:54 -0800

    add some calls to js-precomp

--- src/components/GitHubEmbed.js
+++ src/components/GitHubEmbed.js
@@ -1,6 +1,47 @@
 import React, { useEffect, useState } from "react";
 import CodeBlock from "@theme/CodeBlock";
 
+let _client;
+function getClient() {
+  if (_client) {
+    return _client;
+  }
+
+  const sdkDemoKey = "client-rfLvYGag3eyU0jYW5zcIJTQip7GXxSrhOFN69IGMjvq";
+  _client = new __STATSIG__.PrecomputedEvaluationsClient(sdkDemoKey, {
+    userID: "",
+  });
+
+  _client.on("*", (d) => console.log(d));
+  _client.initializeSync();
+
+  return _client;
+}
+
+function logRender(url, language) {
+  try {
+    getClient().logEvent({
+      eventName: "gh_embed_render",
+      value: url,
+      metadata: { language },
+    });
+  } catch (error) {
+    // noop
+  }
+}
+
+function logFailure(url, language, error) {
+  try {
+    getClient().logEvent({
+      eventName: "gh_embed_error",
+      value: url,
+      metadata: { language, error },
+    });
+  } catch (error) {
+    // noop
+  }
+}
+
 function extractSnippet(input) {
   const parts = input.split("\n");
   const result = [];
@@ -20,16 +61,24 @@ function extractSnippet(input) {
   return result.join("\n");
 }
 
+function runGateCheck() {
+  try {
+    return getClient().checkGate("partial_gate");
+  } catch (error) {
+    // noop
+  }
+
+  return false;
+}
+
 export default function GitHubEmbed({ url, language }) {
   const [content, setContent] = useState("// Loading...");
 
-  try {
-    console.log("StatsigMono", window.__STATSIG__);
-  } catch (e) {
-    console.error(e);
-  }
+  runGateCheck();
 
   useEffect(() => {
+    logRender(url, language);
+
     const fetchData = async () => {
       try {
         const response = await fetch(url);
@@ -40,6 +89,7 @@ export default function GitHubEmbed({ url, language }) {
         setContent(extractSnippet(data));
       } catch (error) {
         setContent(`// Failed to load example.\n// View on GitHub:\n// ${url}`);
+        logFailure(url, language, error);
       }
     };
 
