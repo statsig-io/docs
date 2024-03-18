fb5eb0baf756c5bf6a5a4c418e5af6ee27efe3b7
Daniel Loomb <daniel@statsig.com>
2024-03-18 10:31:48 -0700

    PrecomputedEvaluationClient -> StatsigClient

--- src/components/GitHubEmbed.js
+++ src/components/GitHubEmbed.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import CodeBlock from "@theme/CodeBlock";
 
+const { StatsigClient } = window.Statsig;
+
 let _client;
 function getClient() {
   if (_client) {
@@ -8,7 +10,7 @@ function getClient() {
   }
 
   const sdkDemoKey = "client-rfLvYGag3eyU0jYW5zcIJTQip7GXxSrhOFN69IGMjvq";
-  _client = new __STATSIG__.PrecomputedEvaluationsClient(sdkDemoKey, {
+  _client = new StatsigClient(sdkDemoKey, {
     userID: "",
   });
 
