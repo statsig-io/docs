b514aa5b77c9c740f7345cd05c1b38e95df5019c
crstatsig <cooper@statsig.com>
2024-03-20 11:31:36 -0400

    guide on integrating sidecar and gtm!

--- sidebars.js
+++ sidebars.js
@@ -247,6 +247,7 @@ module.exports = {
             "guides/sidecar-experiments/setup",
             "guides/sidecar-experiments/creating-experiments",
             "guides/sidecar-experiments/publishing-experiments",
+            "guides/sidecar-experiments/integrating-gtm"
           ],
         },
         "holdouts/introduction",
