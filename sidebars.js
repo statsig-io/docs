0360f1fd11afc598bd17055213fb51571eb0fbf1
Daniel Loomb <daniel@statsig.com>
2024-03-24 10:54:56 +1100

    adding expo docs for jsmono

--- sidebars.js
+++ sidebars.js
@@ -248,7 +248,7 @@ module.exports = {
             "guides/sidecar-experiments/setup",
             "guides/sidecar-experiments/creating-experiments",
             "guides/sidecar-experiments/publishing-experiments",
-            "guides/sidecar-experiments/integrating-gtm"
+            "guides/sidecar-experiments/integrating-gtm",
           ],
         },
         "holdouts/introduction",
@@ -432,6 +432,7 @@ module.exports = {
                 // "client/javascript-mono/IntegrationReact",
                 "client/javascript-mono/UsingEvaluationsDataAdapter",
                 "client/javascript-mono/ReactNativeUsage",
+                "client/javascript-mono/ExpoUsage",
                 "client/javascript-mono/Examples",
               ],
             },
