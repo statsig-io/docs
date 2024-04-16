a775be4fe310e95665ea2f12156113bf982f5f2e
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-04-16 11:43:45 -0700

    Add session recording

--- src/components/GitHubEmbed.js
+++ src/components/GitHubEmbed.js
@@ -8,12 +8,15 @@ function getClient() {
   }
 
   const sdkDemoKey = "client-rfLvYGag3eyU0jYW5zcIJTQip7GXxSrhOFN69IGMjvq";
-  const { StatsigClient } = window.Statsig;
+  const { StatsigClient, runStatsigAutoCapture, runStatsigSessionReplay } =
+    window.Statsig;
 
   _client = new StatsigClient(sdkDemoKey, {
     userID: "",
   });
 
+  runStatsigAutoCapture(_client);
+  runStatsigSessionReplay(_client);
   _client.initializeSync();
 
   return _client;
@@ -23,7 +26,7 @@ function logRender(url, language) {
   try {
     getClient().logEvent({
       eventName: "gh_embed_render",
-      value: url,
+      value: url.replace("https://raw.githubusercontent.com/statsig-io", ""),
       metadata: { language },
     });
   } catch (error) {
