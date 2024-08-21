68e8723020c0c0ec00542d4b296ff969a14be742
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2024-08-21 15:30:49 -0700

    Update docusaurus.config.ts (#1930)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,12 +91,6 @@ const config: Config = {
               {
                 tagName: "script",
                 attributes: {
-                  src: "https://cdn.jsdelivr.net/npm/statsig-js-local-eval@1.0.0/build/statsig-prod-web-sdk.min.js",
-                },
-              },
-              {
-                tagName: "script",
-                attributes: {
                   src: "https://cdn.jsdelivr.net/npm/@statsig/js-client@1/build/statsig-js-client+session-replay+web-analytics.min.js?apikey=client-c1fEjmA7JETNimhlFhKf2M1qMRFNkl9ipDfxYEatlYJ",
                 },
               },
