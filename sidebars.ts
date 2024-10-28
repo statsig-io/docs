84c3be1806e4b6bc0089c16fdbdcd68da332940c
vmadhusudanan <31516123+vmadhusudanan@users.noreply.github.com>
2024-10-28 09:30:16 -0700

    re add athena

--- sidebars.ts
+++ sidebars.ts
@@ -678,6 +678,7 @@ const sidebars: SidebarsConfig = {
                     "statsig-warehouse-native/connecting-your-warehouse/snowflake",
                     "statsig-warehouse-native/connecting-your-warehouse/databricks",
                     "statsig-warehouse-native/connecting-your-warehouse/redshift",
+                    "statsig-warehouse-native/connecting-your-warehouse/athena",
                   ],
                 },
                 "statsig-warehouse-native/connecting-your-warehouse/forwarded-data",
