710ec531e7ae38af7c22feedb2d26ad32438c85c
Brock Lumbard <brock@statsig.com>
2025-01-03 15:55:49 -0800

    eject notfound comp. and track instances of 404

--- src/theme/NotFound/index.tsx
+++ src/theme/NotFound/index.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react';
-import NotFound from '@theme-original/NotFound';
-import type NotFoundType from '@theme/NotFound';
-import type {WrapperProps} from '@docusaurus/types';
+import React from 'react';
+import {translate} from '@docusaurus/Translate';
+import {PageMetadata} from '@docusaurus/theme-common';
+import Layout from '@theme/Layout';
+import NotFoundContent from '@theme/NotFound/Content';
 
-type Props = WrapperProps<typeof NotFoundType>;
-
-export default function NotFoundWrapper(props: Props): JSX.Element {
+export default function Index(): JSX.Element {
+  
+  const title = translate({
+    id: 'theme.NotFound.title',
+    message: 'Page Not Found',
+  });
   return (
     <>
-      <NotFound {...props} />
+      <PageMetadata title={title} />
+      <Layout>
+        <NotFoundContent />
+      </Layout>
     </>
   );
 }
