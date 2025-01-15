dec2890af6e9c63fda4836f6ed67eff6494b2012
brock-statsig <146870406+brock-statsig@users.noreply.github.com>
2025-01-14 22:08:03 -0800

    location-rendered page ranking from dynamic config (#2486)
    * location-rendered page ranking from dynamic config
    
    * remove logs
    
    * remove old elements
    
    ---------
    
    Co-authored-by: jlumbard <jlumbard@uwo.ca>

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Layout from '@theme-original/Layout';
 
 export default function CustomLayout(props) {
-  useEffect(() => {
+  const location = useLocation();
 
+  useEffect(() => {
     console.log('Page loaded!');
-  }, []);
+  }, [location]);
+
   useLayoutEffect(() => {
     // Wait a small tick to ensure DOM is fully rendered
     setTimeout(() => {
@@ -14,7 +17,16 @@ export default function CustomLayout(props) {
         console.log("doesn't have sidebar")
         Statsig.instance().logEvent('NoSidebarPageLoad', window.location.href);
       }
+      const docsearchRankingConfig = Statsig.instance().getDynamicConfig('docsearch_ranking_manual');
+      const url = window.location.href.replace(/^https?:\/\//, '');
+      const docsearchRankingManual = docsearchRankingConfig.get(url, 0);
+      document.querySelectorAll('.docsearch-ranking-manual').forEach(el => el.remove());
+      const newElement = document.createElement('div');
+      newElement.setAttribute('ranking', docsearchRankingManual);
+      newElement.classList.add('docsearch-ranking-manual');
+      document.body.appendChild(newElement); //We'll consume this in the algolia crawler
     }, 0);
-  }, []);
+  }, [location]);
+
   return <Layout {...props} />;
 }
