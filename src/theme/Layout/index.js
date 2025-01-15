5a336d5fb3546b0b7e5b5eb0237d93906cb0d8ec
jlumbard <jlumbard@uwo.ca>
2025-01-14 22:02:15 -0800

    location-rendered page ranking from dynamic config

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Layout from '@theme-original/Layout';
 
 export default function CustomLayout(props) {
-  useEffect(() => {
+  const location = useLocation();
 
+  useEffect(() => {
     console.log('Page loaded!');
-  }, []);
+  }, [location]);
+
   useLayoutEffect(() => {
     // Wait a small tick to ensure DOM is fully rendered
     setTimeout(() => {
@@ -14,7 +17,18 @@ export default function CustomLayout(props) {
         console.log("doesn't have sidebar")
         Statsig.instance().logEvent('NoSidebarPageLoad', window.location.href);
       }
+      const docsearchRankingConfig = Statsig.instance().getDynamicConfig('docsearch_ranking_manual');
+      console.log('docsearchRankingConfig', docsearchRankingConfig);
+      const url = window.location.href.replace(/^https?:\/\//, '');
+      console.log('url', url);
+      const docsearchRankingManual = docsearchRankingConfig.get(url, 0);
+      console.log('docsearchRankingManual', docsearchRankingManual);
+      const newElement = document.createElement('div');
+      newElement.setAttribute('ranking', docsearchRankingManual);
+      newElement.classList.add('docsearch-ranking-manual');
+      document.body.appendChild(newElement);
     }, 0);
-  }, []);
+  }, [location]);
+
   return <Layout {...props} />;
 }
