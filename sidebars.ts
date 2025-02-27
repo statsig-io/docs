71ff35a2ef6db6118ce2490aaf318ff272223e75
vmadhusudanan <31516123+vmadhusudanan@users.noreply.github.com>
2025-02-27 06:29:45 -0800

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -390,7 +390,7 @@ const sidebars: SidebarsConfig = {
                 "integrations/event_filtering",
                 "integrations/event_webhook",
                 "integrations/openai",
-                "experimentation/statsig-lite",
+                "experiments/statsig-lite",
                 {
                   type: "category",
                   label: "Migrate from LaunchDarkly",
