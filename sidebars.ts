ec31bb1caa758eff1aa83a4351814aea7a72b1b3
crstatsig <cooper@statsig.com>
2025-03-04 17:38:27 -0500

    sidecar spa doc

--- sidebars.ts
+++ sidebars.ts
@@ -669,6 +669,7 @@ const sidebars: SidebarsConfig = {
                 "guides/sidecar-experiments/setup",
                 "guides/sidecar-experiments/creating-experiments",
                 "guides/sidecar-experiments/measuring-experiments",
+                "guides/sidecar-experiments/advanced-configurations",
                 "guides/sidecar-experiments/publishing-experiments",
                 "guides/sidecar-experiments/integrating-gtm",
                 "guides/aa-sidecar",
