adb24ff479058cd16d4b857c19be308ba5609db9
Andre Terron <andre@statsig.com>
2025-07-24 16:39:29 -0700

    Add icon

--- sidebars.ts
+++ sidebars.ts
@@ -58,8 +58,8 @@ const sidebars: SidebarsConfig = {
               collapsed: true,
               items: [
                 "guides/migrate-from-launchdarkly",
-                "guides/open-source-script", 
-                "guides/ui-based-tool"
+                "guides/open-source-script",
+                "guides/ui-based-tool",
               ],
             },
           ],
@@ -430,7 +430,7 @@ const sidebars: SidebarsConfig = {
           type: "doc",
           id: "session-replay/cli-session-replay",
           label: "CLI Session Replay",
-          className: "terminal-icon sidebar-icon",
+          className: "cli-session-replay-icon sidebar-icon",
         },
         {
           type: "doc",
