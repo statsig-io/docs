a9a7169053182fe1f6b8f4e4d6b8b34fcb6a6776
Brian Barak <brianbarak@statsig.com>
2025-07-25 10:18:06 -0700

    Fix race condition in signup CTA

--- src/components/NavbarItems/SignupCTA/index.js
+++ src/components/NavbarItems/SignupCTA/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import { useExperiment } from "../../lib/statsig";
 
 const SignupCTA = () => (
   <BrowserOnly>
     {() => {
-      const statsig = window.Statsig.StatsigClient.instance();
-      const ctaExp = statsig?.getExperiment('docs_signup_cta');
-      const url = ctaExp?.get('url') ?? 'https://console.statsig.com';
-      const cta = ctaExp?.get('cta') ?? 'Get Started';
+      const ctaExp = useExperiment("docs_signup_cta");
+      const url = ctaExp?.get("url") ?? "https://console.statsig.com";
+      const cta = ctaExp?.get("cta") ?? "Get Started";
       return (
         <a className="navbar__item" href={url} target="_blank">
           <button className="signupCTA CTA">{cta}</button>
